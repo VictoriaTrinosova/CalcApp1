47002f4c34b2f355628121f524224d16886d3863
VICTORIA TRINOSOVA <trinosova.victoria@yandex.ru>
2022-01-22 18:53:13 +0300

    123

--- CalcApp1/MainWindow.xaml.cs
+++ CalcApp1/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,45 @@ namespace CalcApp1
         public MainWindow()
         {
             InitializeComponent();
+            foreach(UIElement el in MainRoot.Children)
+            {
+                if (el is Button)
+                {
+                    ((Button)el).Click += Button_Click;
+                }
+            }
+
+        List<string> styles = new List<string>() { "Темная тема", "Светлая тема" };
+            styleBox.ItemsSource = styles;
+            styleBox.SelectionChanged += ThemeChange;
+            styleBox.SelectedIndex = 0;
+        }
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string str = (string)((Button)e.OriginalSource).Content;
+            if (str == "AC")
+                textBlock.Text = "";
+            else if (str == "=")
+            {
+                string value = new DataTable().Compute(textBlock.Text, null).ToString();
+                textBlock.Text = value;
+            }
+            else
+                textBlock.Text += str;
+
+        }
+        private void ThemeChange(object sender, SelectionChangedEventArgs e)
+        {
+            int styleIndex = styleBox.SelectedIndex;
+            Uri uri = new Uri("Dark.xaml", UriKind.Relative);
+            if (styleIndex == 1)
+            {
+                uri = new Uri("Light.xaml", UriKind.Relative);
+            }
+            ResourceDictionary resource = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resource);
         }
     }
 }
