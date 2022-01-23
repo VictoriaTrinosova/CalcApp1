0d0020d5012d83773a2e167145e1cb48a55e96d0
VICTORIA TRINOSOVA <trinosova.victoria@yandex.ru>
2022-01-23 15:08:05 +0300

    321

--- CalcApp1/MainWindow.xaml.cs
+++ CalcApp1/MainWindow.xaml.cs
@@ -20,50 +20,53 @@ namespace CalcApp1
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
+    
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
-            foreach(UIElement el in MainRoot.Children)
+            //Обработчик событий для всех кнопок на гриде
+            InitializeComponent();
+            foreach (UIElement el in MainRoot.Children)
             {
                 if (el is Button)
                 {
                     ((Button)el).Click += Button_Click;
                 }
             }
-
-        List<string> styles = new List<string>() { "Темная тема", "Светлая тема" };
+            // Добавление выбора темы в ComboBox
+            List<string> styles = new List<string>() { "Синяя тема", "Красная тема" };
             styleBox.ItemsSource = styles;
             styleBox.SelectionChanged += ThemeChange;
             styleBox.SelectedIndex = 0;
         }
-        
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            string str = (string)((Button)e.OriginalSource).Content;
-            if (str == "AC")
-                textBlock.Text = "";
-            else if (str == "=")
-            {
-                string value = new DataTable().Compute(textBlock.Text, null).ToString();
-                textBlock.Text = value;
-            }
-            else
-                textBlock.Text += str;
 
+            private void Button_Click(object sender, RoutedEventArgs e)
+            {
+                string str = (string)((Button)e.OriginalSource).Content; //Создание строковой переменной для вывода названия кнопки в textBlock
+            if (str == "AC") //Реализация кнопки "АС"
+                    textBlock.Text = ""; 
+                else if (str == "=") //Реализация кнопки "="
+            {
+                    string value = new DataTable().Compute(textBlock.Text, null).ToString(); // Использование метода Compute для высчета математической операции, которая передается в виде строки
+                textBlock.Text = value;
+                }
+                else
+                    textBlock.Text += str; //Вывод текста кнопки в textBlock
         }
-        private void ThemeChange(object sender, SelectionChangedEventArgs e)
+        
+        private void ThemeChange(object sender, SelectionChangedEventArgs e) //Событие ThemeChange, срабатывающее при смене выбора
         {
             int styleIndex = styleBox.SelectedIndex;
-            Uri uri = new Uri("Dark.xaml", UriKind.Relative);
-            if (styleIndex == 1)
+            Uri uri = new Uri("Blue.xaml", UriKind.Relative); //По умолчанию синяя тема
+            if (styleIndex == 1) //Определение красной темы
             {
-                uri = new Uri("Light.xaml", UriKind.Relative);
+                uri = new Uri("Red.xaml", UriKind.Relative);
             }
-            ResourceDictionary resource = Application.LoadComponent(uri) as ResourceDictionary;
-            Application.Current.Resources.Clear();
-            Application.Current.Resources.MergedDictionaries.Add(resource);
+            ResourceDictionary resource = Application.LoadComponent(uri) as ResourceDictionary; //Получение ресурса
+            Application.Current.Resources.Clear(); //Отчистка ресурсов
+            Application.Current.Resources.MergedDictionaries.Add(resource); //Добавление словаря
         }
     }
 }
